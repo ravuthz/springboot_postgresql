6246b63728506048398f20e01c04de27352972df
Ravuthz <ravuthz@gmail.com>
2022-08-17 23:30:29 +0700

    Generate Spring Boot from https://start.spring.io

---
+++ src/main/java/com/docker/sb_postgres/SbPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.docker.sb_postgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbPostgresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docker/sb_postgres/SbPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.sb_postgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
